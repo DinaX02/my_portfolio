25aaa0a7bee0854a03b2a317645a386cd664ec1e
DinaX <dinisantunes@ua.pt>
2023-05-14 02:02:56 +0100

    btn_go back

--- src/components/hero_proj1.js
+++ src/components/hero_proj1.js
@@ -1,16 +1,21 @@
 import React from "react";
 import BackgroundKORU  from '../assets/background cover koru app (2).png';
 import setaBack from '../assets/arrow_back_proj.svg'
+import { useParams, useNavigate } from "react-router-dom";
 import "../App.css";
 
 const HeroProject_1 = ()=> {
 
+    const navigate = useNavigate();
+    const goBack = () => {
+        navigate(-1);
+    }
 
     return(
 
 <div className="titulo_proj">
 <header className="header_proj1">
-    <div className="seta_rounded">
+    <div onClick={goBack} className="seta_rounded">
     <img className="seta_back_projects" src={setaBack}/></div>
     <h1>Koru Mobile App </h1>
 </header>
--- src/components/infoProj1.js
+++ src/components/infoProj1.js
@@ -1,8 +1,12 @@
 import React from "react";
 import "../App.css";
+
 import { Link } from "react-router-dom";
 
+
+
 const InfoProj1 = ()=> {
+
 
 
     return(
