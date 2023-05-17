f1ff5784e2274fa977ee9a3b52c428056882cbb9
DinaX <dinisantunes@ua.pt>
2023-05-17 22:25:04 +0100

    nota zetflciks

--- src/components/content_proj2.js
+++ src/components/content_proj2.js
@@ -12,6 +12,8 @@ const ContentP2 = ()=> {
     <div className="content_p111">
         <h1 className="title_overview_2">Zetflicks</h1>
     <p>Zetflicks é um pequeno projeto desenvolvido por 3 estudantes universitários da Universidade de Aveiro com o objetivo de se tornarem mais experientes em novas linguagens de programação e uso de API. O projeto consiste num site responsivo onde o utilizador pode encontrar novos filmes e series, ver alguns detalhes sobre eles e adicioná-los à sua Watchlist.</p>
+
+    <p><strong className="strong_p2">Nota: </strong>O funcionalidade de filtragem por categorias, não está funcional por problemas de Host, a versão 100% funcional encontra-se diretamente no Repositório GitHub.</p>
        <span><h3>Papel desempenhado:</h3><p>UI Design e Front-end</p></span> 
         <h3>Ferramentas usadas:</h3>
         <ul>
