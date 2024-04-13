9f2ae0b79c6010f88620651ddf6abd0c294e4e23
DinaX <dinisantunes@ua.pt>
2024-04-13 23:34:00 +0100

    ajustes

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,36 +1,35 @@
 import React from "react";
-import webDesignicon from "../assets/web design.png"
-import frontendICON from "../assets/icon_front_end.png"
+import webDesignicon from "../assets/web design.png";
+import frontendICON from "../assets/icon_front_end.png";
 import "../App.css";
 
 const Skills = () => {
   const linhaStyle = {
-    marginTop:20,
-    width: '200px',
-    height: '5px',
-    backgroundColor: '#A767FB',
-    position:"absolute",
-    right: '0',
+    marginTop: 20,
+    width: "200px",
+    height: "5px",
+    backgroundColor: "#A767FB",
+    position: "absolute",
+    right: "0",
   };
 
   const numeroStyle = {
-    marginTop:5,
-    position:"absolute",
-    right: '15px',
-    color: '#A767FB',
-    fontSize: '46px',
-    fontWeight: 'bold',
+    marginTop: 5,
+    position: "absolute",
+    right: "15px",
+    color: "#A767FB",
+    fontSize: "46px",
+    fontWeight: "bold",
   };
 
   return (
     <div className="skills_div">
-
-<div className="linhasss_roxos">
-      <div style={linhaStyle}>
-        <p style={numeroStyle}>02</p>
+      <div className="linhasss_roxos">
+        <div style={linhaStyle}>
+          <p style={numeroStyle}>02</p>
+        </div>
       </div>
-      </div>
-{/* 
+      {/* 
       <p  id="skills" className="introduction">
         A nível Técnico{" "}
       </p>
@@ -39,48 +38,127 @@ const Skills = () => {
 
     <h1 className="big_title">Skills.</h1>
     </div> */}
-    <div className="instagram-container">
-    <div className="instagram-info"> 
-    <p className="paragrafo_intro_sizes">A nível Técnico</p>
+      <div className="instagram-container">
+        <div className="instagram-info">
+          <p className="paragrafo_intro_sizes">A nível Técnico</p>
 
-<p className="fontsize_tags"><span className="title_marcado_koru">Skills.</span></p>
-   
-     <p style={{marginTop:40, fontSize:19}}>Através do  <span className="cor_nome_2">Front-end</span> e  <span className="cor_nome_2">UX/UI Design</span>, sou capaz de criar experiências envolventes e únicas no utilizador , combinando o design com o conhecimento técnico para o desenvolvimento de soluções web que cativam e satisfazem os seus utilizadores.
-     </p>
+          <p className="fontsize_tags">
+            <span className="title_marcado_koru">Skills.</span>
+          </p>
 
-
-   </div>
-   <div className="other-info">
-{/* <img className="img_vision" src={imgVisaoGeRAL}/>  */}
-      </div></div>
-
+          <p style={{ marginTop: 40, fontSize: 19 }}>
+            Através do{" "}
+            <span className="title_marcado_skills">UX/UI Design</span> e do{" "}
+            <span className="title_marcado_skills">
+              Desenvolvimento Front-end
+            </span>
+            , sou capaz de criar experiências envolventes e únicas no utilizador
+            , combinando o design com o conhecimento técnico para o
+            desenvolvimento de soluções web que cativam e satisfazem os seus
+            utilizadores.
+          </p>
+        </div>
+        <div className="other-info">
+          {/* <img className="img_vision" src={imgVisaoGeRAL}/>  */}
+        </div>
+      </div>
 
       <section className="skills_geral skills section">
         <div className="skills_container container grid">
           <div>
             <div className="skills_content">
-              <div className="skills_header space_bg_ecra">
 
-<div className="arrow_nome">
-<img className="icons_skills" src={frontendICON} alt="IconFrontEnd"/>
-  <h1 className="skills_title">Desenvolvimento Front-end</h1>
- {/* <img className="arrow_ajust" src={arrowDown}/>*/}
-</div>
-<div className="skills_list grid">
+            <div className="skills_content">
+                <div className="skills_header space_bg_ecra_2">
+                  <div className="arrow_nome">
+                    <img
+                      className="icons_skills"
+                      src={webDesignicon}
+                      alt="IconWebDesign"
+                    />
+                    <h1 className="skills_title">UX/ UI Design</h1>
+                    {/*  <img className="arrow_ajust" src={arrowDown}/>*/}
+                  </div>
+                  <div className="skills_list grid">
+                    <div className="skills_data">
+                      <div className="skills_titles">
+                        <h3 className="skills_name">Sketching</h3>
+                        <span className="skills_number">90%</span>
+                      </div>
 
-  <div className="skills_data">
+                      <div className="skills_bar">
+                        <span className="skills_percentage skills_sketch"></span>
+                      </div>
+                    </div>
+
+                    <div className="skills_data">
+                      <div className="skills_titles">
+                        <h3 className="skills_name">Figma</h3>
+                        <span className="skills_number">90%</span>
+                      </div>
+
+                      <div className="skills_bar">
+                        <span className="skills_percentage skills_sketch"></span>
+                      </div>
+                    </div>
+
+                    <div className="skills_data">
+                      <div className="skills_titles">
+                        <h3 className="skills_name">Invision</h3>
+                        <span className="skills_number">75%</span>
+                      </div>
+                      <div className="skills_bar">
+                        <span className="skills_percentage skills_invision"></span>
+                      </div>
+                    </div>
+
+                    <div className="skills_data">
+                    <div className="skills_titles">
+                      <h3 className="skills_name">Illustrator</h3>
+                      <span className="skills_number">85%</span>
+                    </div>
+                    <div className="skills_bar">
+                      <span className="skills_percentage skills_bootstrap"></span>
+                    </div>
+                  </div>
+
+                    {/* <div className="skills_data">
     <div className="skills_titles">
-      <h3 className="skills_name">HTML5</h3>
-      <span className="skills_number">90%</span>
+      <h3 className="skills_name">Photoshop</h3>
+      <span className="skills_number">60%</span>
     </div>
-
-
     <div className="skills_bar">
-    <span className="skills_percentage skills_html"></span>
+    <span className="skills_percentage skills_photoshop"></span>
     </div>
-  </div>
+  </div> */}
+                  </div>
+                </div>
+              </div>
 
-  {/* <div className="skills_data">
+
+              <div className="skills_header space_bg_ecra">
+                <div className="arrow_nome">
+                  <img
+                    className="icons_skills"
+                    src={frontendICON}
+                    alt="IconFrontEnd"
+                  />
+                  <h1 className="skills_title">Desenvolvimento Front-end</h1>
+                  {/* <img className="arrow_ajust" src={arrowDown}/>*/}
+                </div>
+                <div className="skills_list grid">
+                  <div className="skills_data">
+                    <div className="skills_titles">
+                      <h3 className="skills_name">HTML5</h3>
+                      <span className="skills_number">90%</span>
+                    </div>
+
+                    <div className="skills_bar">
+                      <span className="skills_percentage skills_html"></span>
+                    </div>
+                  </div>
+
+                  {/* <div className="skills_data">
     <div className="skills_titles">
       <h3 className="skills_name">CSS3</h3>
       <span className="skills_number">80%</span>
@@ -90,106 +168,38 @@ const Skills = () => {
     </div>
   </div> */}
 
-  <div className="skills_data">
-    <div className="skills_titles">
-      <h3 className="skills_name">CSS3 e Bootstrap</h3>
-      <span className="skills_number">85%</span>
-    </div>
-    <div className="skills_bar">
-    <span className="skills_percentage skills_bootstrap"></span>
-    </div>
-  </div>
+                  <div className="skills_data">
+                    <div className="skills_titles">
+                      <h3 className="skills_name">CSS3 e Bootstrap</h3>
+                      <span className="skills_number">85%</span>
+                    </div>
+                    <div className="skills_bar">
+                      <span className="skills_percentage skills_bootstrap"></span>
+                    </div>
+                  </div>
 
-  <div className="skills_data">
-    <div className="skills_titles">
-      <h3 className="skills_name">JavaScript</h3>
-      <span className="skills_number">70%</span>
-    </div>
-    <div className="skills_bar">
-    <span className="skills_percentage skills_javascript"></span>
-    </div>
-  </div>
+                  <div className="skills_data">
+                    <div className="skills_titles">
+                      <h3 className="skills_name">JavaScript</h3>
+                      <span className="skills_number">80%</span>
+                    </div>
+                    <div className="skills_bar">
+                      <span className="skills_percentage skills_illustrator"></span>
+                    </div>
+                  </div>
 
-  <div className="skills_data">
-    <div className="skills_titles">
-      <h3 className="skills_name">React</h3>
-      <span className="skills_number">80%</span>
-    </div>
-    <div className="skills_bar">
-    <span className="skills_percentage skills_react"></span>
-    </div>
-  </div>
-</div>
-
+                  <div className="skills_data">
+                    <div className="skills_titles">
+                      <h3 className="skills_name">React</h3>
+                      <span className="skills_number">85%</span>
+                    </div>
+                    <div className="skills_bar">
+                      <span className="skills_percentage skills_bootstrap"></span>
+                    </div>
+                  </div>
+                </div>
               </div>
-           
 
-
-            <div className="skills_content">
-              <div className="skills_header space_bg_ecra_2">
-
-                <div className="arrow_nome">
-<img className="icons_skills" src={webDesignicon }  alt="IconWebDesign" />
-  <h1 className="skills_title">UX/ UI Design</h1>
- {/*  <img className="arrow_ajust" src={arrowDown}/>*/}
-</div>
-<div className="skills_list grid">
-
-  <div className="skills_data">
-    <div className="skills_titles">
-      <h3 className="skills_name">Sketching</h3>
-      <span className="skills_number">90%</span>
-    </div>
-
-
-    <div className="skills_bar">
-    <span className="skills_percentage skills_sketch"></span>
-    </div>
-  </div>
-
-  <div className="skills_data">
-    <div className="skills_titles">
-      <h3 className="skills_name">Figma</h3>
-      <span className="skills_number">80%</span>
-    </div>
-    <div className="skills_bar">
-    <span className="skills_percentage skills_figma"></span>
-    </div>
-  </div>
-
-  <div className="skills_data">
-    <div className="skills_titles">
-      <h3 className="skills_name">Invision</h3>
-      <span className="skills_number">75%</span>
-    </div>
-    <div className="skills_bar">
-    <span className="skills_percentage skills_invision"></span>
-    </div>
-  </div>
-
-  <div className="skills_data">
-    <div className="skills_titles">
-      <h3 className="skills_name">Illustrator</h3>
-      <span className="skills_number">80%</span>
-    </div>
-    <div className="skills_bar">
-    <span className="skills_percentage skills_illustrator"></span>
-    </div>
-  </div>
-
-  {/* <div className="skills_data">
-    <div className="skills_titles">
-      <h3 className="skills_name">Photoshop</h3>
-      <span className="skills_number">60%</span>
-    </div>
-    <div className="skills_bar">
-    <span className="skills_percentage skills_photoshop"></span>
-    </div>
-  </div> */}
-  
-</div>
-</div>
-              </div>
             </div>
           </div>
         </div>
